7c066d56ed2c5c7f662d1980a9174ee4963ea0d0
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-06-21 13:52:01 -0400

    feat (router): Se implemento el router del modulo de user auth
    - se establecion el enrutamiento de los endpoints de user auth
      - ruta post/ --> para realizar el login normal con jwt
      - ruta post/ --> para cerrar seccion
      - ruta get/ --> para hacer login con google
      - ruta get/ --> para redireccion de google en caso de exito o error
      - ruta post/ --> para cerrar seccion con google
    
    archivos modificados::
    - src/modules/user/auth/router/auth.js

---
+++ backend/src/modules/user/auth/router/auth.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { AuthController } from '../controller/auth.js';
+import passport from 'passport';
+
+export const authRouter = Router();
+
+authRouter.post('/login', AuthController.login);
+authRouter.post('/logout', AuthController.logout);
+
+authRouter.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+authRouter.get('/google/callback',
+  passport.authenticate('google', { 
+    failureRedirect: '/login',
+    successRedirect: 'http://localhost:3000/' // Redirige al frontend
+  })
+);
+
+authRouter.post('/google/logout', AuthController.googleLogout)
