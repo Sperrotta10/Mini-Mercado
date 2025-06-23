5da9301a5714ea713dcf36df623275e3eaf3f6b8
Juan Wu <juanwu8238@gmail.com>
2025-06-23 17:08:47 -0400

    Agrega Componente Carta_producto para optimizar los codigos

---
+++ frontend/Mini_mercado/src/views/home/components/Carta_producto.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="producto_carta">
+        <img :src="imagen" alt="Producto" class="producto_carta_imagen">
+        <div class="product_informacion">
+
+            <h3 class="producto_nombre">{{ nombre }}</h3>
+            <div class="producto_precio">{{precio}}</div>
+
+            <button class="btn_agregar_carrito">Agregar carrito</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Producto_carta_funcion',
+  props: {
+    imagen: {
+      type: String,
+      required: true
+    },
+    nombre: {
+      type: String,
+      required: true
+    },
+    precio: {
+      type: [Number, String],
+      required: true
+    }
+  },
+//   methods: { //Esto quizas es para el futuro
+//     Agregar_al_carrito() {
+//       this.$emit('Agregar_al_carrito', {
+//         nombre: this.nombre,
+//         precio: this.precio,
+//         imagen: this.imagen
+//       });
+//     }
+//   }
+}
+</script>
+
+<style scoped>
+.producto_carta {
+    width: 160px;
+    background: white;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.08);
+    transition: all 0.3s ease;
+}
+
+.producto_carta:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.15);
+}
+
+.producto_carta_imagen {
+    width: 100%;
+    height: 180px;
+    object-fit: cover;
+    border-bottom: 1px solid #eee;
+}
+
+.producto_informacion {
+    padding: 15px;
+}
+
+.producto_nombre {
+    font-size: 1rem;
+    font-weight: 600;
+    margin-bottom: 8px;
+    height: 40px;
+    overflow: hidden;
+}
+
+.producto_precio {
+    color: #004C45;
+    font-weight: 700;
+    font-size: 1.2rem;
+    margin: 10px 0;
+}
+
+
+.btn_agregar_carrito {
+    width: 100%;
+    background: #10b68d;
+    color: white;
+    border: none;
+    padding: 8px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: 500;
+    margin-top: 5px;
+    transition: all 0.3s ease;
+}
+
+.btn_agregar_carrito:hover {
+    background: #018175;
+}
+</style>
--- frontend/Mini_mercado/src/views/home/view/pagina_principal.vue
+++ frontend/Mini_mercado/src/views/home/view/pagina_principal.vue
@@ -20,22 +20,72 @@
 
     <main>
         <div class="contendor_producto">
-            <div class="producto_carta">
-                <img :src="pizza" alt="Producto" class="producto_carta_imagen">
-                <div class="product_informacion">
-                    <h3 class="producto_nombre">Pizza</h3>
-                    
-                    <div class="producto_precio">¥19.90</div>
-                    <button class="btn_agregar_carrito">Agregar carrito</button>
-                </div>
-            </div>
+            <!--Podemos que llamar 2 formas los componentes, para que si el futuro quieran hacer-->
+            <!--Con el ciclo for-->
+            <Carta_producto 
+                v-for="producto in productos" 
+                :key="producto.id"
+                :imagen="producto.imagen"
+                :nombre="producto.nombre"
+                :precio="producto.precio"
+            ></Carta_producto>
+
+            <!--O simplemente llenar los datos-->
+            <Carta_producto :nombre="'Pizza_mas'" :precio="234" :imagen="pizzaImg"></Carta_producto>
         </div>
     </main>
 </template>
 
+<script >
+import Carta_producto from '../components/Carta_producto.vue'
+
+export default {
+   //Aqui tiene que nombrar el nombre de componente para llamarlo
+  components: {
+    Carta_producto  
+  },
+  data() {
+    return {
+      productos: [
+        {
+          id: 1,
+          nombre: 'Pizza',
+          precio: '19.90',
+          imagen: pizzaImg
+        },
+        {
+          id: 2,
+          nombre: 'Pizza',
+          precio: '15.90',
+          imagen: pizzaImg
+        },
+        {
+          id: 3,
+          nombre: 'Pizza',
+          precio: '15.90',
+          imagen: pizzaImg
+        },
+        {
+          id: 4,
+          nombre: 'Pizza',
+          precio: '15.90',
+          imagen: pizzaImg
+        },
+        {
+          id: 5,
+          nombre: 'Pizza',
+          precio: '15.90',
+          imagen: pizzaImg
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <script setup>
 import logo from '@/assets/Imagenes/LogoMSJ.png'
-import pizza from '@/assets/Imagenes/pizza jamon.png'
+import pizzaImg from '@/assets/Imagenes/pizza jamon.png'
 </script>
 
 <style scoped>
