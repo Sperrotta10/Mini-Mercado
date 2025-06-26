e340b885106d7e9a3099ec9a3a3b93c25db62d25
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-06-26 16:39:59 -0400

    refactor (controller and router): Se modifico el controlador y el router del modulo de producto
    - Se cambio la logica del metodo update ya que habia un error al actualizar el producto
      debido a que se estaba usando mal el nombre del atributo role y era role
    
    - Se implemento el middleware de authentification para el endpoint de actualizar producto
    
    archivo modificado:
    - src/modules/product/controller/product.js
    - src/modules/product/router/product.js

--- backend/src/modules/product/controller/product.js
+++ backend/src/modules/product/controller/product.js
@@ -49,20 +49,20 @@ export class ProductController extends BaseController {
 
     // sobrescribiendo el método update
     update = async (req, res) => {
-        const { role } = req.user;
+        const { rol } = req.user;
         const { id } = req.params;
         const result = this.validators.update(req.body);
 
-        if (!result.success)
-        return res.status(400).json({ message: "Error de validación", error: result.error.errors });
+        if (!result.success) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
 
+        console.log("Role del usuario:", rol);
         try {
-            if (role === 'admin') {
+            if (rol === 'admin') {
                 
                 const updated = await this.model.update(id, result.data);
                 return res.status(updated.status).json({ message: updated.message, data: updated.data });
 
-            } else if (role === 'empleado') {
+            } else if (rol === 'empleado') {
                 
                 const filtered = { stock: result.data.stock };
                 const updated = await this.model.update(id, filtered);
@@ -75,4 +75,44 @@ export class ProductController extends BaseController {
             return res.status(500).json({ message: "Error interno", error: error.message });
         }
     };
+
+    // sobrescribiendo el método create (para poder crear un proucto o un array de productos)
+    create = async (req, res) => {
+        const data = req.body;
+
+        try {
+            // Si es un array, procesamos en lote
+            if (Array.isArray(data)) {
+                const results = [];
+
+                for (const user of data) {
+                    const validation = this.validators.create(user);
+
+                    if (!validation.success) {
+                        return res.status(400).json({ message: "Error de validación", error: validation.error.errors });
+                    }
+
+                    const result = await this.model.create(validation.data);
+                    results.push({ success: true, data: result.data });
+                }
+
+                return res.status(207).json({ message: 'Procesamiento por lote', results });
+            }
+
+            // Si es solo un objeto
+            const validation = this.validators.create(data);
+
+            if (!validation.success) {
+            return res.status(400).json({ message: 'Error de validación', errors: validation.error.errors });
+            }
+
+            const producto = await this.model.create(validation.data);
+            return res.status(producto.status).json({ message: producto.message, data: producto.data });
+
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Error al crear producto(s)', error });
+        }
+    }
+
 }
--- backend/src/modules/product/router/product.js
+++ backend/src/modules/product/router/product.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ProductController } from "../controller/product.js";
+import { authenticateHybrid } from "../../../middlewares/auth/authentificate.js"
 
 export const productRouter = Router();
 const controller = new ProductController();
@@ -9,6 +10,6 @@ productRouter.get('/search/:name', controller.searchByName);
 productRouter.get('/', controller.getAll);
 productRouter.get('/:id', controller.getId);
 productRouter.post('/', controller.create);
-productRouter.put('/:id', controller.update);
+productRouter.patch('/:id', authenticateHybrid, controller.update);
 productRouter.delete('/:id', controller.delete);
 
