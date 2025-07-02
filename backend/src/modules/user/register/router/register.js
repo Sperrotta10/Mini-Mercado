52d1d37c7c0fd4f501f23d962ec6078661564eab
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-02 17:45:46 -0400

    feat (router module user.register): Se implemento la ruta para el metodo de obtener empleados por paginacion

--- backend/src/modules/user/register/router/register.js
+++ backend/src/modules/user/register/router/register.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { UserRegisterController } from '../controller/register.js';
+import { authenticateHybrid } from '../../../../middlewares/auth/authentificate.js';
+import { authorizeRoles } from '../../../../middlewares/auth/authRole.js'
 
 export const registerRouter = Router();
 const controller = new UserRegisterController();
 
 // definicion de rutas para el registro de usuarios
-registerRouter.post('/', controller.create);
+registerRouter.post('/', authenticateHybrid, authorizeRoles('admin', 'cliente'), controller.create);
 registerRouter.get('/', controller.getAll);
 registerRouter.get('/cedula/:cedula', controller.getCedula);
+registerRouter.get('/empleado/pagination', controller.getPaginationEmpleado);
 registerRouter.get('/:id', controller.getId);
 registerRouter.patch('/:id', controller.update);
 registerRouter.delete('/:id', controller.delete);
