2010395c29c361b756cb18b01a26ec89a89c6712
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-02 17:38:04 -0400

    feat and refactoe (controller module user/register): Se implemento funcion para obtener los empleado y se modifico el metodo create
    - El metodo para obtener usuarios:
      - Se implemento validacion para las query params (page, limit, role)
    
    - El metodo create se sobreescibio para modificar el funcionamiento:
      - se implemento el manejo de roles, ahora se puede crear un usuario con un rol especifico,
        si es admin puede crear un empleado, y si no tiene rol significa que es un cliente
      - se implemento la validacion de los roles, si el rol no existe se retorna un error.
    
    archivos modificados:
    - backend/src/modules/user/register/controller/register.js

--- backend/src/modules/user/register/controller/register.js
+++ backend/src/modules/user/register/controller/register.js
@@ -1,7 +1,10 @@
 import { BaseController } from '../../../../controller/controller.js';
 import { validateCreateUser } from "../validation/create.js"
 import { validateUpdateUser } from "../validation/update.js";
+import { validatePagination } from '../validation/get_pagination.js';
+import { resolveRole } from '../helpers/roleHelper.js';
 import { ModelUserRegister } from "../model/register.js"
+import { enviroment } from '../../../../config/enviroment.js';
 
 export class UserRegisterController extends BaseController {
 
@@ -11,6 +14,33 @@ export class UserRegisterController extends BaseController {
       update: validateUpdateUser
     });
   }
+
+  create = async (req, res) => {
+    const { rol } = req.user || {};
+    const { role, ...userData } = req.body;
+    const result = this.validators.create(userData);
+
+    if (!result.success) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
+
+    try {
+
+      let roleResult;
+      if(rol === 'admin') {
+
+        roleResult = await resolveRole(role);
+        if (!roleResult.success) return res.status(roleResult.status).json({ message: roleResult.message });
+
+      }
+
+      const rol_id = rol === 'admin' ? roleResult.rol_id : enviroment.ROLE_DEFAULT;
+
+      const created = await this.model.create(result.data, rol_id);
+      return res.status(created.status).json({ message: created.message, data: created.data ?? null });
+
+    } catch (error) {
+      return res.status(500).json({ message: "Error interno", error: error.message });
+    }
+  };
 
   getCedula = async (req, res) => {
 
@@ -25,5 +55,25 @@ export class UserRegisterController extends BaseController {
       return res.status(500).json({ message: "Error interno", error: error.message });
     }
   };
+
+  getPaginationEmpleado = async (req, res) => {
+
+    const result = validatePagination(req.query);
+
+    if (!result.success) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
+
+    const { page, limit, role } = result.data;
+    const offset = (page - 1) * limit;
+
+    try {
+      
+      const data = await this.model.getPaginationEmpleado(page, limit, offset, role);
+      return res.status(data.status).json({ message: data.message, data: data.data ?? null});
+
+    } catch (error) {
+      return res.status(500).json({ message: "Error interno", error: error.message });
+      
+    }
+  }
   
 }
