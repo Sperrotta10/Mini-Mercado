ce95d58ed0a33bdbb9d9ffa3664e492414267bc9
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-05 12:11:52 -0400

    feat (router module category): Se cambio el endpoint de update en vez de put a pach

--- backend/src/modules/category/router/category.js
+++ backend/src/modules/category/router/category.js
@@ -6,5 +6,5 @@ const controller = new CategoryController();
 
 categoriaRouter.get("/", controller.getAll);
 categoriaRouter.post("/", controller.create);
-categoriaRouter.put("/:id", controller.update);
+categoriaRouter.patch("/:id", controller.update);
 categoriaRouter.delete("/:id", controller.delete);
