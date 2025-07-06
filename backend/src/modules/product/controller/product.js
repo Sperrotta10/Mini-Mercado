20a44ed8471cd1e581bed3d56b3314a7cf475127
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-05 20:36:26 -0400

    refactor (controller module product): Se modifico el funcionamiento de create y update
    - metodo create:
      - Se implemento una mejor validacion para la imagen y los datos del body
      - Se implemento un mejor manejo de errores
      - Se implemento la subida de imagen en supaBase
      - Se manejo de errores al subir la imagen
      - Se implemento un mejor manejo de errores al crear el producto
    
    - metodo update:
      - Se implemento una mejor validacion para la imagen y los datos del body
      - Se implemento un mejor manejo de errores
      - Se implemento la subida de imagen en supaBase
      - Se manejo de errores al subir la imagen
      - Se implemento un mejor manejo de errores al actualizar el producto
    
      archivos modificados:
      - backend/src/modules/product/controller/product.js

--- backend/src/modules/product/controller/product.js
+++ backend/src/modules/product/controller/product.js
@@ -1,6 +1,10 @@
 import { BaseController } from "../../../controller/controller.js";
 import { validateCreateProduct } from "../validation/create.js"
 import { validateUpdateProduct } from "../validation/update.js"
+import { validatePagination } from "../validation/get_pagination.js"
+import { isImageFile } from "../../../helpers/images.js"
+import { errorUploadImage } from "../../../helpers/upload_image.js"
+import { uploadImage, deleteFile, extractPathFromUrl } from "../../../utils/images.js"
 import { ProductModel } from "../model/product.js"
 
 export class ProductController extends BaseController {
@@ -12,24 +16,67 @@ export class ProductController extends BaseController {
         });
     }
 
-    getPaginatedWithFilters = async (req, res) => {
-        
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
-        const offset = (page - 1) * limit;
+    create = async (req, res) => {
 
-        // aplicacion de filtros
-        const filters = {
-            minPrice: req.query.minPrice ? parseFloat(req.query.minPrice) : null,
-            maxPrice: req.query.maxPrice ? parseFloat(req.query.maxPrice) : null,
-            minOferta: req.query.minOferta ? parseInt(req.query.minOferta) : null,
-            maxOferta: req.query.maxOferta ? parseInt(req.query.maxOferta) : null,
-            categoria: req.query.categoria ? parseInt(req.query.categoria) : null,
-        };
+        const image = req.files?.image;
+
+        if (image) {
+            const resultImage = isImageFile(image);
+            if (!resultImage.success) return res.status(resultImage.status).json({ message: resultImage.message });
+        }
+
+        const validate = this.validators?.create;
+        const result = validate ? validate(req.body) : { success: true, data: req.body };
+
+        if (!result.success) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
+
+        const payload = result.data;
+        const FOLDER = 'productos';
+        const BUCKET = 'product-images';
+        let filePath = null;
 
         try {
-            const result = await this.model.getPaginatedWithFilters(page, limit, offset, filters);
-            return res.status(result.status).json({ message: result.message, data: result.data });
+
+            // subir imagen a Supabase
+            const imageUrl = await uploadImage(image, FOLDER, BUCKET);
+
+            if (!imageUrl) return res.status(500).json({ message: "Error al subir la imagen" });
+
+            filePath = extractPathFromUrl(imageUrl);
+            payload.image = imageUrl; // agregamos la URL de la imagen en los datos
+
+            // guardar el producto en la base de datos
+            const created = await this.model.create(payload);
+
+            if (created.status === 409) {
+                
+                console.error("El producto ya existe:", created.message, filePath);
+                await errorUploadImage(filePath, BUCKET);
+            }
+
+            return res.status(created.status).json({ message: created.message, data: created.data ?? null });
+
+        } catch (error) {
+
+            await errorUploadImage(filePath, BUCKET);
+
+            return res.status(500).json({ message: "Error interno", error: error.message });
+        }
+    };
+
+    getPaginatedWithFilters = async (req, res) => {
+        
+        const result = validatePagination(req.query);
+
+        if (!result.success) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
+
+        const { page, limit, ...filters} = result.data;
+
+        const offset = (page - 1) * limit;
+
+        try {
+            const response = await this.model.getPaginatedWithFilters(page, limit, offset, filters);
+            return res.status(response.status).json({ message: response.message, data: response.data });
         } catch (error) {
             return res.status(500).json({ message: "Error interno", error: error.message });
         }
@@ -49,18 +96,68 @@ export class ProductController extends BaseController {
 
     // sobrescribiendo el método update
     update = async (req, res) => {
+
+        const image = req.files?.image;
+        const body = req.body || {};
+
+        if (Object.keys(body).length === 0 && !image) return res.status(400).json({ message: "No se proporcionaron datos para actualizar el producto" });
+
+        if (image) {
+            const resultImage = isImageFile(image);
+            if (!resultImage.success) return res.status(resultImage.status).json({ message: resultImage.message });
+        }
+
         const { rol } = req.user;
         const { id } = req.params;
-        const result = this.validators.update(req.body);
+        const result = this.validators.update(body);
 
         if (!result.success) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
 
         console.log("Role del usuario:", rol);
+        const payload = result.data;
+        let filePath = null;
+        const FOLDER = 'productos';
+        const BUCKET = 'product-images';
+
         try {
             if (rol === 'admin') {
+
+                try {
+                 
+                    // subir imagen a Supabase si se proporciona
+                    const imageUrl = await uploadImage(image, FOLDER, BUCKET);
+
+                    if (!imageUrl) return res.status(500).json({ message: "Error al subir la imagen" });
+
+                    filePath = extractPathFromUrl(imageUrl);
+                    payload.image = imageUrl; // agregamos la URL de la imagen en los datos
+
+                    const product = await this.model.getId(id);
+
+                    if (product.status !== 200) {
+
+                        await errorUploadImage(filePath, BUCKET);
+
+                        return res.status(product.status).json({ message: product.message, data: product.data ?? null });
+                    }
                 
-                const updated = await this.model.update(id, result.data);
-                return res.status(updated.status).json({ message: updated.message, data: updated.data });
+                    // actualizar el producto en la base de datos
+                    const updated = await this.model.update(id, payload);
+                    if (updated.status === 200) {
+                        
+                        if (filePath && product.data.image && product.data.image !== payload.image) {
+                            await deleteFile(product.data.image, BUCKET);
+                        }
+                        return res.status(updated.status).json({ message: updated.message, data: updated.data ?? null });
+                    }
+                    return res.status(updated.status).json({ message: updated.message, data: updated.data });
+
+                } catch (error) {
+
+                    await errorUploadImage(filePath, BUCKET);
+
+                    return res.status(500).json({ message: "Error interno del servidor", error: error.message });
+                }
 
             } else if (rol === 'empleado') {
                 
@@ -77,7 +174,7 @@ export class ProductController extends BaseController {
     };
 
     // sobrescribiendo el método create (para poder crear un proucto o un array de productos)
-    create = async (req, res) => {
+    createAll = async (req, res) => {
         const data = req.body;
 
         try {
