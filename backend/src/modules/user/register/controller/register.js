d0982914ca49df4d98ae86d92b28df38746ffbf6
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-26 22:16:28 -0400

    feat (controller module user/register): Se sobrescribio el metodo update de las superClase, para manejar de manera adecuada la implementacion de imagenes en el perfil del usuario, validaciones, manejo de errores y documentacion detallada del funcionamiento del codigo

--- backend/src/modules/user/register/controller/register.js
+++ backend/src/modules/user/register/controller/register.js
@@ -5,6 +5,9 @@ import { validatePagination } from '../validation/get_pagination.js';
 import { resolveRole } from '../helpers/roleHelper.js';
 import { ModelUserRegister } from "../model/register.js"
 import { enviroment } from '../../../../config/enviroment.js';
+import { isImageFile } from '../../../../helpers/images.js';
+import { uploadImage, extractPathFromUrl, deleteFile } from '../../../../utils/images.js';
+import { errorUploadImage } from '../../../../helpers/upload_image.js';
 
 export class UserRegisterController extends BaseController {
 
@@ -74,6 +77,70 @@ export class UserRegisterController extends BaseController {
       return res.status(500).json({ message: "Error interno", error: error.message });
       
     }
+  }
+
+  update = async (req, res) => {
+
+    const image = req.files?.image_perfil;
+    const body = req.body || {};
+
+    if (Object.keys(body).length === 0 && !image) return res.status(400).json({ message: "No se proporcionaron datos para actualizar el usuario" });
+
+    if (image) {
+        const resultImage = isImageFile(image);
+        if (!resultImage.success) return res.status(resultImage.status).json({ message: resultImage.message });
+    }
+
+    const { id } = req.params;
+    const result = this.validators.update(body);
+
+    if (!result.success && !image) return res.status(400).json({ message: "Error de validación", error: result.error.errors });
+
+    const payload = result.data || {};
+    let filePath = null;
+    const FOLDER = 'perfiles';
+    const BUCKET = 'perfil-images';
+
+    try {
+
+      if (image) {
+
+        // subir imagen a Supabase si se proporciona
+        const imageUrl = await uploadImage(image, FOLDER, BUCKET);
+
+        // verificar si la URL de la imagen se obtuvo correctamente
+        if (!imageUrl) return res.status(500).json({ message: "Error al subir la imagen" });
+
+        // extraer la ruta del archivo de la URL
+        filePath = extractPathFromUrl(imageUrl);
+        payload.image_perfil = imageUrl; // agregamos la URL de la imagen en los datos
+      }
+
+      const updated = await this.model.update(id, payload);
+
+      // si la actualización falla y hay una imagen, eliminarla
+      if(updated.status !== 200) {
+        await errorUploadImage(filePath, BUCKET); // eliminar imagen si ocurre un error
+      }
+
+      const user = await this.model.getId(id);
+
+      // si la actualización fue exitosa y el usuario tiene una imagen, eliminar la imagen anterior
+      if (user.status === 200 && user.data?.image_perfil) {
+        const imageBeforeUpdate = user.data.image_perfil;
+        await deleteFile(imageBeforeUpdate, BUCKET);
+      }
+
+      return res.status(updated.status).json({ message: updated.message, data: updated.data ?? null });
+      
+    } catch (error) {
+
+      // Si ocurre un error, revertir la carga de la imagen
+      await errorUploadImage(filePath, BUCKET);
+
+      return res.status(500).json({ message: "Error interno", error: error.message });
+    }
+
   }
   
 }
