037dbc91bd682c8497655c32a0c23a5ae6b960f3
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-29 15:09:09 -0400

    feat (model module user/auth): Se implemento toda la logica de negocio y validaciones necesarias para los metodos forgotPassword and resetPassword

--- backend/src/modules/user/auth/model/auth.js
+++ backend/src/modules/user/auth/model/auth.js
@@ -1,7 +1,8 @@
-import { User, Role } from "../../../../models/index.js"
+import { User, Role, PasswordRecovery } from "../../../../models/index.js"
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { enviroment } from "../../../../config/enviroment.js";
+import { sendRecoveryEmail } from "../helper/sendEmail.js";
 
 export class AuthModel {
 
@@ -19,6 +20,8 @@ export class AuthModel {
             });
 
             if(!user) return {message : "El email no existe", status : 401};
+
+            if(!user.status) return {message : "Usuario inactivo", status : 401};
 
             const verifyPassword = await bcrypt.compare(data.password, user.password);
 
@@ -39,4 +42,69 @@ export class AuthModel {
             throw error;
         }
     }
+
+    static async forgotPassword(data) {
+
+        try {
+
+            const userExists = await User.findOne({ where: { email: data.email } });
+
+            if (!userExists) return { message: "El email no existe", status: 404 };
+            if (!userExists.status) return { message: "Usuario inactivo", status: 401 };
+
+            const token = crypto.randomUUID();
+            const expiresAt = new Date(Date.now() + 15 * 60 * 1000); // 15 minutos
+
+            await PasswordRecovery.create({
+                user_id: userExists.user_id,
+                token,
+                expires_at: expiresAt
+            });
+
+            await sendRecoveryEmail(userExists.email, token);
+
+            return { message: "Se ha enviado un email de recuperación", status: 200 };
+
+        } catch (error) {
+            console.error("Error al hacer el forgot password", error.message);
+            throw error;
+        }
+    }
+
+    static async resetPassword(data) {
+
+        try {
+
+            const newPassword = data.password;
+            const token = data.token;
+
+            const recoveryRecord = await PasswordRecovery.findOne({ where: { token } });
+
+            if (!recoveryRecord) return { message: "Token inválido", status: 400 };
+            
+            // Verificar si ya fue usado
+            if (recoveryRecord.used) return { message: "Este enlace ya fue utilizado", status: 400 };
+            
+
+            // Verificar expiración
+            if (new Date() > recoveryRecord.expires_at) return { message: "El token ha expirado", status: 400 };
+            
+            // Cambiar contraseña
+            const salt = parseInt(enviroment.SALT, 10);
+            const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+            await User.update({ password: hashedPassword }, { where: { user_id: recoveryRecord.user_id } });
+
+            // Marcar el token como usado
+            recoveryRecord.used = true;
+            await recoveryRecord.save();
+
+            return { message: "Contraseña actualizada exitosamente", status: 200 };
+
+        } catch (error) {
+            console.error("Error al hacer resetPassword:", error.message);
+            throw error;
+        }
+    }
+
 }
