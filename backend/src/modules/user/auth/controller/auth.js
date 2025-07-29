5a4bcd3c2841b1c676bd83e2ac81f6a340e9aa97
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-29 15:08:03 -0400

    feat (controller module user/auth): Se implemento los metodos forgotPassword and resetPassword para la recuperacion de password y se agrego cierto parametro para la creacion de la cookie en el metodo de login

--- backend/src/modules/user/auth/controller/auth.js
+++ backend/src/modules/user/auth/controller/auth.js
@@ -1,10 +1,10 @@
-//import { BaseController } from '../../../../controller/controller.js';
 import { validateLoginData } from "../validation/login.js"
+import { validateEmail } from "../validation/email.js";
+import { validateResetPasswordData } from "../validation/reset_password.js";
 import { AuthModel } from '../model/auth.js';
 import { enviroment } from '../../../../config/enviroment.js';
 
 export class AuthController {
-
 
     static async login(req,res) {
 
@@ -21,11 +21,53 @@ export class AuthController {
             res.cookie('access_token', login.access_token, {
                 httpOnly : true, // la cookie solo se puede acceder en el servidor
                 secure : enviroment.SECURE_COOKIE === 'production', // la cookie solo se puede acceder en https
-                maxAge : 1000 * 60 * 60 // duracion de una hora
+                maxAge : 1000 * 60 * 60, // duracion de una hora
+                sameSite: 'strict', // la cookie solo se envia al mismo sitio
+                signed: true // la cookie esta firmada
             });
 
             return res.status(200).json({message : login.message, data : login.data});
             
+        } catch (error) {
+
+            return res.status(500).json({message : "Error interno del servidor", error : error.message});
+        }
+    }
+
+    static async forgotPassword(req, res) {
+        
+        const result = validateEmail(req.body);
+
+        if(!result.success) return res.status(422).json({message : "Error de validacion", error : result.error.errors});
+
+        try {
+
+            const forgotPassword = await AuthModel.forgotPassword(result.data);
+
+            if(forgotPassword.status) return res.status(forgotPassword.status).json({message : forgotPassword.message});
+
+            return res.status(200).json({message : forgotPassword.message, data : forgotPassword.data});
+
+        } catch (error) {
+
+            return res.status(500).json({message : "Error interno del servidor", error : error.message});
+        }
+    }
+
+    static async resetPassword(req, res) {
+
+        const result = validateResetPasswordData(req.body);
+
+        if(!result.success) return res.status(422).json({message : "Error de validacion", error : result.error.errors});
+
+        try {
+
+            const resetPassword = await AuthModel.resetPassword(result.data);
+
+            if(resetPassword.status) return res.status(resetPassword.status).json({message : resetPassword.message});
+
+            return res.status(200).json({message : resetPassword.message, data : resetPassword.data});
+
         } catch (error) {
 
             return res.status(500).json({message : "Error interno del servidor", error : error.message});
