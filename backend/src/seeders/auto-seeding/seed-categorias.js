16c23d1c6c3951411082e04bc150538067b9995d
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-29 16:54:21 -0400

    feat (seeders): Se implemento una estructura mas eficiente para los seeders y se implemento la image para el modulo de la categoria

--- backend/src/seeders/auto-seeding/seed-categorias.js
+++ backend/src/seeders/auto-seeding/seed-categorias.js
@@ -1,8 +1,27 @@
 import { Category } from '../../models/index.js';
 
+const imageCategory = {
+  imgLacteos: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753574219176_lacteos.jpg",
+  imgFrutasVerduras: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753574319538_frutas_y_verduras.webp",
+  imgCarniceria: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753574403116_carne.jpg",
+  imgAbarrotes: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753574446080_abarrotes.jpg",
+  imgBebidas: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753574471691_refresco.jpg",
+  imgSnacksDulces: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753574496351_chucheria.jpg",
+  imgLimpieza: "https://mkxlawuamztbmwbfxugr.supabase.co/storage/v1/object/public/category-images/category/1753581692754_limpieza2.jpg"
+}
+
 export async function seedCategories() {
-  const categories = ['Lácteos', 'Frutas y Verduras', 'Carnicería', 'Abarrotes', 'Bebidas', 'Snacks y Dulces', 'Limpieza'];
-  for (const name of categories) {
-    await Category.findOrCreate({ where: { name } });
+
+  try {
+    const categories = [{ name : 'Lácteos', image : imageCategory.imgLacteos}, { name : 'Frutas y Verduras', image : imageCategory.imgFrutasVerduras}, { name : 'Carnicería', image : imageCategory.imgCarniceria}, { name : 'Abarrotes', image : imageCategory.imgAbarrotes}, { name : 'Bebidas', image : imageCategory.imgBebidas}, { name : 'Snacks y Dulces', image : imageCategory.imgSnacksDulces}, { name : 'Limpieza', image : imageCategory.imgLimpieza}];
+    for (const { name, image } of categories) {
+      await Category.findOrCreate({ where: { name }, defaults: { image } });
+    }
+
+    console.log("✅ Categorías seed cargadas correctamente.");
+  } catch (error) {
+    console.error("Error seeding categories:", error);
+    throw error;
+    
   }
 }
--- backend/src/seeders/auto-seeding/seed-roles.js
+++ backend/src/seeders/auto-seeding/seed-roles.js
@@ -1,8 +1,17 @@
 import { Role } from '../../models/index.js';
 
 export async function seedRoles() {
-  const roles = ['cliente', 'empleado', 'admin'];
-  for (const name of roles) {
-    await Role.findOrCreate({ where: { name } });
+
+  try {
+    const roles = ['cliente', 'empleado', 'admin'];
+    for (const name of roles) {
+      await Role.findOrCreate({ where: { name } });
+    }
+
+    console.log("✅ Roles seed cargados correctamente.");
+  } catch (error) {
+    console.error("Error seeding roles:", error);
+    throw error;
+    
   }
 }
