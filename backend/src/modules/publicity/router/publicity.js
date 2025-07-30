27b4a0f1d69a834e48c28bc30ce50bf6e9ce8529
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-30 14:45:38 -0400

    feat (middleware rate-limit module publicity): Se implemento una proteccion para que cada endpoint tenga un limite de peticiones por minutos

--- backend/src/modules/publicity/router/publicity.js
+++ backend/src/modules/publicity/router/publicity.js
@@ -2,12 +2,13 @@ import { Router } from "express"
 import { PublicityController } from "../controller/publicity.js";
 import { authenticateHybrid } from "../../../middlewares/auth/authentificate.js"
 import { authorizeRoles } from "../../../middlewares/auth/authRole.js"
+import { getLimiter, modifyLimiter } from "../../../middlewares/rate_limiters/publicity/publicity.js";
 
 export const publicityRouter = Router();
 const publicityController = new PublicityController();
 
-publicityRouter.post("/", authenticateHybrid, authorizeRoles("admin"), publicityController.create);
-publicityRouter.get("/", publicityController.getAll);
-publicityRouter.get("/:id", publicityController.getId);
-publicityRouter.put("/:id", authenticateHybrid, authorizeRoles("admin"), publicityController.update);
-publicityRouter.delete("/:id", authenticateHybrid, authorizeRoles("admin"), publicityController.delete);
+publicityRouter.post("/", authenticateHybrid, authorizeRoles("admin"), modifyLimiter, publicityController.create);
+publicityRouter.get("/", getLimiter, publicityController.getAll);
+publicityRouter.get("/:id", getLimiter, publicityController.getId);
+publicityRouter.put("/:id", authenticateHybrid, authorizeRoles("admin"), modifyLimiter, publicityController.update);
+publicityRouter.delete("/:id", authenticateHybrid, authorizeRoles("admin"), modifyLimiter, publicityController.delete);
