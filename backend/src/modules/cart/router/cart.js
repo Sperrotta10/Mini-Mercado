e4e0cd260f4120c45be393ad4ff4a9dd8a167b7f
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-30 14:44:40 -0400

    feat (middleware rate-limit module cart): Se implemento una proteccion para que cada endpoint tenga un limite de peticiones por minutos

--- backend/src/modules/cart/router/cart.js
+++ backend/src/modules/cart/router/cart.js
@@ -3,13 +3,14 @@ import { CartController } from "../controller/cart.js"
 import { authenticateHybrid } from "../../../middlewares/auth/authentificate.js"
 import { authorizeRoles } from "../../../middlewares/auth/authRole.js"
 import { verifyCartOwnership } from "../../../middlewares/cart_shopping/authCart.js"
+import { createLimiter, deleteLimiter, readLimiter, updateLimiter } from "../../../middlewares/rate_limiters/cart/cart.js"
 
 export const cartRouter = Router()
 
 const cartController = new CartController()
 
-cartRouter.post("/", authenticateHybrid, authorizeRoles('cliente'), cartController.create)
-cartRouter.get("/", authenticateHybrid, authorizeRoles('cliente'), cartController.getAll)
-cartRouter.get("/:id", authenticateHybrid, authorizeRoles('cliente'), verifyCartOwnership, cartController.getId)
-cartRouter.put("/:id", authenticateHybrid, authorizeRoles('cliente'), verifyCartOwnership, cartController.update)
-cartRouter.delete("/:id", authenticateHybrid, authorizeRoles('cliente'), verifyCartOwnership, cartController.delete)
+cartRouter.post("/", authenticateHybrid, authorizeRoles('cliente'), createLimiter, cartController.create)
+cartRouter.get("/", authenticateHybrid, authorizeRoles('cliente'), readLimiter, cartController.getAll)
+cartRouter.get("/:id", authenticateHybrid, authorizeRoles('cliente'), verifyCartOwnership, readLimiter, cartController.getId)
+cartRouter.put("/:id", authenticateHybrid, authorizeRoles('cliente'), verifyCartOwnership, updateLimiter, cartController.update)
+cartRouter.delete("/:id", authenticateHybrid, authorizeRoles('cliente'), verifyCartOwnership, deleteLimiter, cartController.delete)
