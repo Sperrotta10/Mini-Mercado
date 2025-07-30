5a4bfb3c162d0e73b9830bed51b9932239e71194
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-30 12:21:09 -0400

    feat (schemas module publicity): Se implemento la tabla para este modulo

---
+++ backend/src/modules/publicity/schemas/publicity.js
@@ -0,0 +1,16 @@
+
+export function definePublicity(sequelize, DataTypes) {
+    const Publicity = sequelize.define('Publicity', {
+        publicity_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return Publicity;
+}
