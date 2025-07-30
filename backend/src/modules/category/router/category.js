61c05edff62db959d327a8e3801f7e78e60413d3
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-30 14:44:59 -0400

    feat (middleware rate-limit module category): Se implemento una proteccion para que cada endpoint tenga un limite de peticiones por minutos

--- backend/src/modules/category/router/category.js
+++ backend/src/modules/category/router/category.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { CategoryController } from '../controller/category.js';
 import { authenticateHybrid } from '../../../middlewares/auth/authentificate.js';
 import { authorizeRoles } from '../../../middlewares/auth/authRole.js';
+import { getLimiter, modifyLimiter } from '../../../middlewares/rate_limiters/category/category.js';
 
 export const categoriaRouter = Router();
 const controller = new CategoryController();
 
-categoriaRouter.get("/", controller.getAll);
-categoriaRouter.get("/:id", controller.getId);
-categoriaRouter.post("/", authenticateHybrid, authorizeRoles('admin'), controller.create);
-categoriaRouter.patch("/:id", authenticateHybrid, authorizeRoles('admin'), controller.update);
-categoriaRouter.delete("/:id", authenticateHybrid, authorizeRoles('admin'), controller.delete);
+categoriaRouter.post("/", authenticateHybrid, authorizeRoles('admin'), modifyLimiter, controller.create);
+categoriaRouter.get("/", getLimiter, controller.getAll);
+categoriaRouter.get("/:id", getLimiter, controller.getId);
+categoriaRouter.patch("/:id", authenticateHybrid, authorizeRoles('admin'), modifyLimiter, controller.update);
+categoriaRouter.delete("/:id", authenticateHybrid, authorizeRoles('admin'), modifyLimiter, controller.delete);
