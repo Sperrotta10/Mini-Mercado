b35d9fcdf6bb707741679766e5ec088a9cc1ba7c
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-30 14:45:18 -0400

    feat (middleware rate-limit module product): Se implemento una proteccion para que cada endpoint tenga un limite de peticiones por minutos

--- backend/src/modules/product/router/product.js
+++ backend/src/modules/product/router/product.js
@@ -2,16 +2,17 @@ import { Router } from 'express';
 import { ProductController } from "../controller/product.js";
 import { authenticateHybrid } from "../../../middlewares/auth/authentificate.js"
 import { authorizeRoles } from '../../../middlewares/auth/authRole.js';
+import { getListLimiter, modifyLimiter } from '../../../middlewares/rate_limiters/product/product.js';
 
 export const productRouter = Router();
 const controller = new ProductController();
 
-productRouter.get('/pagination', controller.getPaginatedWithFilters);
-productRouter.get('/search/:name', controller.searchByName);
-productRouter.get('/', controller.getAll);
-productRouter.get('/:id', controller.getId);
-productRouter.post('/all', authenticateHybrid, authorizeRoles('admin'), controller.createAll); // ruta temporal
-productRouter.post('/', authenticateHybrid, authorizeRoles('admin'), controller.create);
-productRouter.patch('/:id', authenticateHybrid, authorizeRoles('admin', 'empleado'), controller.update);
-productRouter.delete('/:id', authenticateHybrid, authorizeRoles('admin'), controller.delete);
+productRouter.post('/all', authenticateHybrid, authorizeRoles('admin'), modifyLimiter, controller.createAll); // ruta temporal
+productRouter.post('/', authenticateHybrid, authorizeRoles('admin'), modifyLimiter, controller.create);
+productRouter.get('/pagination', getListLimiter, controller.getPaginatedWithFilters);
+productRouter.get('/search/:name', getListLimiter, controller.searchByName);
+productRouter.get('/', getListLimiter, controller.getAll);
+productRouter.get('/:id', getListLimiter, controller.getId);
+productRouter.patch('/:id', authenticateHybrid, authorizeRoles('admin', 'empleado'), modifyLimiter, controller.update);
+productRouter.delete('/:id', authenticateHybrid, authorizeRoles('admin'), modifyLimiter, controller.delete);
 
