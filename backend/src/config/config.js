3922bb690ff1942e20c71490153ba04642e4434d
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-01 15:25:32 -0400

    config (sequelize-cli): Se implemento la configuracion necesaria para que funcione correctamente sequelize-cli, para asi poder trabajar con migraciones y seeders. Se utilizo la extension .cjs ya que sequelize-cli trabaja con CommonJs

--- backend/src/config/config.js
+++
@@ -1,9 +0,0 @@
-import { enviroment } from "./enviroment.js";
-
-export const development = {
-    username: enviroment.DB_USER,
-    password: enviroment.DB_PASSWORD,
-    database: enviroment.DB_NAME,
-    host: enviroment.DB_HOST,
-    dialect: "mysql"
-};
