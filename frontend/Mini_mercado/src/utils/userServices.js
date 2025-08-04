ff33bd81e97f890c5dc265741ca27eaaaba789cc
Manu <manueldavidnava31@gmail.com>
2025-08-04 14:03:48 -0400

    feat(auth): Add AuthStore for session authentication + Methods in userServices for authentication

--- frontend/Mini_mercado/src/utils/userServices.js
+++ frontend/Mini_mercado/src/utils/userServices.js
@@ -21,4 +21,37 @@ export class UserService {
         }
     }
 
+    async login(data) {
+        try {
+            const res = await this.api.post('/auth/login/', data)
+            return {status:true, data: res.data}
+        } catch (error) {
+            console.error('Error al iniciar sesión:', error)
+            return false
+        }
+    }
+
+    async logout() {
+        try {
+        const res = await api.post('/auth/logout', {})
+        if (res.status === 200) return true
+        } catch (error) {
+        console.error('Error al cerrar sesión:', error)
+        }
+        return false
+    }
+
+    async getSectionId(){
+        try {
+        const res = await api.get('/auth/me')
+        if (res.status === 200) {
+            return { status: true, data: res.data.data }
+        }
+        } catch (error) {
+        console.error('Error al obtener sección por ID:', error)
+        return false
+        }
+        return false
+    }
+
 }
