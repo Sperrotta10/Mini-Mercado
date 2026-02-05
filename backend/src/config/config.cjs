3f49b5219f8e93496378a3ed7a87414bab0011a1
Sperrotta10 <santiagoperrotta10@gmail.com>
2026-02-04 20:37:50 -0400

    feat(seed): utilizar función para generar URLs de imágenes en categorías

--- backend/src/config/config.cjs
+++ backend/src/config/config.cjs
@@ -29,6 +29,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectOptions: (() => {
         const ssl = getMySqlSslOptions();
